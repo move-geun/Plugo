776ae56db1f189c561b8cefbf7fd4f8662cdabe4
이동근 <movegeun11@gmail.com>
2023-05-30 05:24:16 +0900

    add : admin item update page

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,12 +1,38 @@
 import React, { useState, useEffect } from "react";
 import http from "../api/http";
 import { ItemID } from "../recoil/atoms";
-import { useRecoilState } from "recoil";
-import { Link } from "react-router-dom";
+import { useRecoilState, useRecoilValue } from "recoil";
+// import { itemDetailSelector } from "../recoil/selector";
+import { Link, useNavigate } from "react-router-dom";
+import { AdminContainer, ItemCard } from "./AdminPage.style";
 
 const AdminPage = () => {
   const [items, setItems] = useState(null);
+  const [updateItem, setUpdateItem] = useState(1);
   const [itemID, setItemId] = useRecoilState(ItemID);
+
+  const item_detail = () => {
+    http
+      .get(`/items/${itemID}`)
+      .then((res) => {
+        setUpdateItem(res.data);
+        setForm({
+          id: res.data.id,
+          title: res.data.title,
+          stock: res.data.stock,
+          price: res.data.price,
+          img: res.data.img,
+          detail: res.data.detail,
+          detail2: res.data.detail2,
+          size: res.data.size,
+          pound: res.data.pound,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   const item_list = () => {
     http
       .get("/items")
@@ -17,34 +43,158 @@ const AdminPage = () => {
         console.log(err);
       });
   };
+
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    id: updateItem.id,
+    title: updateItem.title,
+    stock: updateItem.stock,
+    price: updateItem.price,
+    img: updateItem.img,
+    detail: updateItem.detail,
+    detail2: updateItem.detail2,
+    size: updateItem.size,
+    pound: updateItem.pound,
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
+
+  const itemUpdate = () => {
+    fetch(`http://localhost:4000/items/${updateItem.id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(form),
+    })
+      .then((res) => res.json())
+      .catch((err) => {
+        console.log(err);
+      });
+    navigate("/admin");
+  };
+
+  const itemlDelete = () => {
+    fetch(`http://localhost:4000/items/${updateItem.id}`, {
+      method: "DELETE",
+    }).then(() => {
+      console.log("delete item");
+    });
+    navigate("/admin");
+  };
+
   useEffect(() => {
     item_list();
-  }, []);
+    item_detail();
+  }, [itemID]);
 
   return (
     <div>
-      "Admin Page"
-      <div>
-        <div>등록된 상품</div>
-        {items
-          ? items.map((item, idx) => (
-              <Link
-                to={`/update/${item.id}/${item.title}`}
-                key={idx}
-                onClick={(e) => setItemId(item.id)}
-              >
-                <div>
-                  <div>{item.title}</div>
-                  <img src={item.img} alt="이미지가 없습니다" />
-                  <div>{item.price}</div>
-                </div>
-              </Link>
-            ))
-          : null}
-      </div>
       <Link to={"/addproduct"}>
         <button>상품 등록하기</button>
       </Link>
+      <AdminContainer>
+        <div className="leftContainer">
+          <div className="title">등록된 상품</div>
+          <div>
+            {items
+              ? items.map((item, idx) => (
+                  <ItemCard key={idx} onClick={(e) => setItemId(item.id)}>
+                    <img src={item.img} alt="이미지가 없습니다" />
+                    <div className="flexCol">
+                      <div>{item.title}</div>
+                      <div>{item.price}</div>
+                    </div>
+                  </ItemCard>
+                ))
+              : null}
+          </div>
+        </div>
+        <div className="rightContainer">
+          상품 디테일
+          <div>
+            <form onSubmit="">
+              <div>
+                <label htmlFor="title">
+                  상품명
+                  <input
+                    type="text"
+                    name="title"
+                    value={"" || form.title}
+                    onChange={handleChange}
+                  />
+                </label>
+                <label htmlFor="stock">
+                  재고
+                  <input
+                    type="text"
+                    name="stock"
+                    value={"" || form.stock}
+                    onChange={handleChange}
+                  />
+                </label>
+                <label htmlFor="price">
+                  가격
+                  <input
+                    type="text"
+                    name="price"
+                    value={"" || form.price}
+                    onChange={handleChange}
+                  />
+                </label>
+                <label htmlFor="img">
+                  이미지 링크
+                  <input
+                    type="text"
+                    name="img"
+                    value={"" || form.img}
+                    onChange={handleChange}
+                  />
+                </label>
+                <label htmlFor="detail">
+                  상품설명
+                  <input
+                    type="text"
+                    name="detail"
+                    value={"" || form.detail}
+                    onChange={handleChange}
+                  />
+                </label>
+                <label htmlFor="detail2">
+                  상품설명2
+                  <input
+                    type="text"
+                    name="detail2"
+                    value={"" || form.detail2}
+                    onChange={handleChange}
+                  />
+                </label>
+                <label htmlFor="size">
+                  사이즈
+                  <input
+                    type="text"
+                    name="size"
+                    value={"" || form.size}
+                    onChange={handleChange}
+                  />
+                </label>
+                <label htmlFor="pound">
+                  무게
+                  <input
+                    type="text"
+                    name="pound"
+                    value={"" || form.pound}
+                    onChange={handleChange}
+                  />
+                </label>
+              </div>
+              <button onClick={itemUpdate}>저장</button>
+            </form>
+            <button onClick={itemlDelete}>아이템 삭제</button>
+          </div>
+        </div>
+      </AdminContainer>
     </div>
   );
 };
--- src/pages/AdminPage.style.js
+++ src/pages/AdminPage.style.js
@@ -3,6 +3,44 @@ import styled from "styled-components";
 const AdminContainer = styled.div`
   width: 90%;
   margin: 0 auto;
+  height: 500px;
+  display: flex;
+  flex-direction: row;
+
+  .leftContainer {
+    width: 500px;
+    background-color: white;
+    overflow: auto;
+    .title {
+      font-size: 20px;
+      text-align: center;
+      margin-top: 10px;
+    }
+  }
+  .rightContainer {
+    width: 550px;
+    background-color: white;
+  }
 `;
 
-export { AdminContainer };
+const ItemCard = styled.div`
+  display: flex;
+  margin-top: 10px;
+  img {
+    width: 70px;
+    height: 70px;
+    margin: 0 20px;
+  }
+  padding-bottom: 10px;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+  border-radius: 5px;
+
+  .flexCol {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    opacity: 0.8;
+  }
+`;
+
+export { AdminContainer, ItemCard };
