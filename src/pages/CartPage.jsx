2851159610deaab26ce0aafa3335b6d5506ed10d
이동근 <movegeun11@gmail.com>
2023-05-30 00:25:49 +0900

    add : get cart list

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,5 +1,35 @@
+import React, { useEffect, useState } from "react";
+import http from "../api/http";
+
 const CartPage = () => {
-  return <div>"Cart Page"</div>;
+  const [cartItems, setCartItems] = useState(null);
+  const cart_list = () => {
+    http
+      .get("/cart")
+      .then((res) => {
+        setCartItems(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    cart_list();
+  }, []);
+
+  return (
+    <div>
+      "Cart Page"
+      {cartItems
+        ? cartItems.map((cart, idx) => (
+            <div key={idx}>
+              <div>{cart.id}</div>
+            </div>
+          ))
+        : null}
+    </div>
+  );
 };
 
 export default CartPage;
