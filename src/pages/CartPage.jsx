772a6446d55433940794c42c375b22aa447897a1
이동근 <movegeun11@gmail.com>
2023-05-30 04:03:22 +0900

    css : cart page

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,18 +1,22 @@
 import React, { useEffect, useState } from "react";
 import http from "../api/http";
+import { CartContainer, ItemContainer, ItemCard } from "./CartPage.style";
 
 const CartPage = () => {
   const [cartItems, setCartItems] = useState(null);
+  const [cartLen, setCartLen] = useState(null);
   const cart_list = () => {
     http
       .get("/cart")
       .then((res) => {
         setCartItems(res.data);
+        setCartLen(res.data.length);
       })
       .catch((err) => {
         console.log(err);
       });
   };
+
   const itemlDelete = (id) => {
     fetch(`http://localhost:4000/cart/${id}`, {
       method: "DELETE",
@@ -27,23 +31,55 @@ const CartPage = () => {
   }, []);
 
   return (
-    <div>
-      "Cart Page"
-      {cartItems
-        ? cartItems.map((cart, idx) => (
-            <div key={idx}>
-              <div>
-                <div>{cart.id}</div>
-                <div>{cart.title}</div>
-                <img src={cart.img} alt="" />
-              </div>
-              <button onClick={() => itemlDelete(cart.id)}>
-                장바구니에서 삭제
-              </button>
+    <CartContainer>
+      <div className="title">내 장바구니 ( {cartLen} )</div>
+      <ItemContainer>
+        <div className="leftContainer">
+          <div className="checkbox">
+            <input type="checkbox" name="allCheck" />
+            <label htmlFor="allCheck">모두 선택</label>
+            <input type="checkbox" name="allCancle" />
+            <label htmlFor="allCancle">모두 해제</label>
+          </div>
+          {cartItems ? (
+            cartItems.map((cart, idx) => (
+              <ItemCard key={idx}>
+                <div className="top">
+                  <div>
+                    <input type="checkbox" name="check" className="itemCheck" />
+                    <label htmlFor="check"></label>
+                  </div>
+                  <div className="card">
+                    <img src={cart.img} alt="" />
+                  </div>
+                  <div className="itemDetail">
+                    <div className="itemStock">{cart.stock}</div>
+                    <div className="itemTitle">{cart.title}</div>
+                    <div className="itemPrice">{cart.price}</div>
+                  </div>
+                </div>
+                <div className="bottom">
+                  <div onClick={() => itemlDelete(cart.id)} className="delBtn">
+                    장바구니 삭제
+                  </div>
+                  <div className="orderBtn">
+                    <button>-</button>
+                    <div>1</div>
+                    <button>+</button>
+                  </div>
+                </div>
+              </ItemCard>
+            ))
+          ) : (
+            <div>
+              장바구니가 비었습니다
+              <div>쇼핑하러 가기</div>
             </div>
-          ))
-        : null}
-    </div>
+          )}
+        </div>
+        <div className="rightContainer">오른쪽</div>
+      </ItemContainer>
+    </CartContainer>
   );
 };
 
--- src/pages/CartPage.style.js
+++ src/pages/CartPage.style.js
@@ -1,8 +1,140 @@
 import styled from "styled-components";
 
 const CartContainer = styled.div`
-  width: 90%;
+  width: 1000px;
   margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  .title {
+    width: 100%;
+    text-align: left;
+    margin-left: 120px;
+    font-size: 20px;
+  }
 `;
 
-export { CartContainer };
+const ItemContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  .leftContainer {
+    width: 650px;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+
+    .checkbox {
+      font-size: 17px;
+      margin-top: 20px;
+      margin-bottom: 20px;
+      margin-left: 50px;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+
+      input {
+        margin-right: 5px;
+        width: 20px;
+        height: 20px;
+        background-color: #4d4022;
+      }
+      label {
+        margin-right: 10px;
+      }
+    }
+  }
+
+  .rightContainer {
+    width: 330px;
+  }
+`;
+
+const ItemCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 500px;
+  background-color: white;
+  border-radius: 5px;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+  margin-left: 55px;
+  img {
+    width: 48px;
+    height: 48px;
+  }
+
+  .top {
+    margin-top: 10px;
+    display: flex;
+    flex-direction: row;
+
+    .itemCheck {
+      width: 20px;
+      height: 20px;
+      margin: 0 10px;
+    }
+    .itemDetail {
+      height: 50px;
+      margin-left: 10px;
+      font-size: 14px;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-evenly;
+
+      .itemStock {
+      }
+      .itemTitle {
+        color: #5b4f33;
+      }
+      .itemPrice {
+        color: #5b4f33;
+        opacity: 0.5;
+      }
+    }
+
+    .card {
+      display: flex;
+      flex-direction: row;
+    }
+  }
+
+  .bottom {
+    width: 500px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+
+    .delBtn {
+      margin: 10px 0 10px 10px;
+      display: flex;
+      flex-direction: column;
+      text-align: center;
+      justify-content: center;
+      border: 1px solid #5b4f33;
+      padding: 0 10px;
+      border-radius: 10px;
+    }
+    .delBtn:hover {
+      background-color: #8c754e;
+      opacity: 0.8;
+    }
+
+    .orderBtn {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      margin-right: 20px;
+      button {
+        background-color: white;
+        font-size: 25px;
+        margin: 0 10px;
+      }
+    }
+  }
+`;
+
+export { CartContainer, ItemContainer, ItemCard };
