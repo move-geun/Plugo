506938e16f9234b07e704a03fae5260efec4b514
이동근 <movegeun11@gmail.com>
2023-05-29 23:46:44 +0900

    add detail data

--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -1,5 +1,34 @@
+import { ItemID } from "../recoil/atoms";
+import { useRecoilValue } from "recoil";
+import { useState, useEffect } from "react";
+import http from "../api/http";
+
 const ProductDetailPage = () => {
-  return <div>"ProductDetailPage"</div>;
+  const nowItem = useRecoilValue(ItemID);
+  const [nowDetail, setNowDetail] = useState(null);
+  const item_detail = () => {
+    http
+      .get(`/items/${nowItem}`)
+      .then((res) => {
+        setNowDetail(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  useEffect(() => {
+    item_detail();
+  }, []);
+
+  return (
+    <div>
+      <div>
+        {nowDetail.id}
+        {nowDetail.title}
+        {nowDetail.detail}
+      </div>
+    </div>
+  );
 };
 
 export default ProductDetailPage;
